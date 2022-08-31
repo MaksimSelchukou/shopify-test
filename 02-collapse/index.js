5360145cbb32e5085391769efe9fbc3c8c1e115d
maksimselchukou <selchukouwebdev1995@gmail.com>
2022-08-31 14:43:54 +0300

    feat:add js code,works with WebAnimationAPI

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,24 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const openContentBtn = document.querySelector('.collapsible__action--hidden')
+const closedContentBtn = document.querySelector('.collapsible__action--visible')
+let content = document.querySelector('.collapsible__content')
+
+
+const contentCollapseToggle = (visibility) => new KeyframeEffect(
+    content,
+    [{visibility}],
+    {duration: 200, fill: 'forwards'}
+)
+
+const collapsedAnimation = new Animation(contentCollapseToggle('hidden'), document.timeline);
+// console.log(collapsedAnimation)
+const unCollapsedAnimation = new Animation(contentCollapseToggle('visible'), document.timeline);
+
+closedContentBtn.addEventListener('click', () => {
+    unCollapsedAnimation.cancel()
+    collapsedAnimation.finish();
+})
+
+openContentBtn.addEventListener('click', () => {
+    collapsedAnimation.cancel()
+    unCollapsedAnimation.finish()
+})
